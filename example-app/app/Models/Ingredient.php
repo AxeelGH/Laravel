184a8d1836a0e0e2d73cf2ed4bee7593293176a9
AxeelGH <axelpg2004@gmail.com>
2026-03-05 10:33:11 +0100

    Feat: Added Controller and apis for endpoints

---
+++ example-app/app/Models/Ingredient.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+  use HasFactory;
+
+  protected $table = 'ingredients';
+  protected $primaryKey = 'ingredient_id';
+  public $timestamps = false;
+
+  protected $fillable = ['ingredient_name', 'price', 'rarity'];
+
+  public function potions(){
+
+    return $this->belongsToMany(Potion::class, 'potion_ingredients', 'ingredient_id', 'potion_id')
+                ->withPivot('qty');
+  }
+
+}
